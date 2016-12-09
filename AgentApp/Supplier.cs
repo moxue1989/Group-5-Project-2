f101a9c8220589a7386c83dadb048fda9d64f4e8
750118 <750118@ICTVM-FQQ06UJG2>
2016-12-09 14:52:52 -0700

    changes

--- AgentApp/Supplier.cs
+++ AgentApp/Supplier.cs
@@ -8,5 +8,14 @@ namespace AgentApp
 {
     public class Supplier
     {
+        int Supplier_ID { get; set; }
+        string Supplier_Name { get; set; }
+
+        //constructor with default initial values provided
+        public Supplier(int id = 0, string name = "")
+        {
+            Supplier_ID = id;
+            Supplier_Name = name;
+        }
     }
 }
