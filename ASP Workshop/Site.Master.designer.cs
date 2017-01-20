74c6c3ae0ccf6129480378292071eb17ea54392c
ACDM\750118 <mo_xue1989@yahoo.ca>
2017-01-20 14:49:22 -0700

    logout button

--- ASP Workshop/Site.Master.designer.cs
+++ ASP Workshop/Site.Master.designer.cs
@@ -49,6 +49,15 @@ namespace ASP_Workshop {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl LogoutOption;
         
         /// <summary>
+        /// btnLogout control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnLogout;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
