536312f74bb06f4b56e28e6e2b49217ae50acb0b
750118 <750118@ICTVM-FQQ06UJG2>
2017-01-20 09:10:16 -0700

    updates

--- ASP Workshop/Customer.aspx.designer.cs
+++ ASP Workshop/Customer.aspx.designer.cs
@@ -29,7 +29,6 @@ namespace ASP_Workshop {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lblMessage;
-        
         /// <summary>
         /// txtFirstName control.
         /// </summary>
