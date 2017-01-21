1ebeeb0376f6432989b51d0c16654453dd3eab9d
cloudbomber <kasiemmanuel@gmail.com>
2017-01-21 13:40:12 -0700

    updated gitignore file

--- ASP Workshop/CustomerLanding.aspx.cs
+++ ASP Workshop/CustomerLanding.aspx.cs
@@ -2,52 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace ASP_Workshop
 {
-    public partial class WebForm2 : System.Web.UI.Page
+    public partial class WebForm4 : System.Web.UI.Page
     {
-        List<Booking> bookings = new List<Booking>();
-        List<BookingDetail> details = new List<BookingDetail>();
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (Session["Customer"] != null)
-            {
-                var customer = (Customer) Session["Customer"];
-                Session["CustomerId"] = customer.CustomerId;
 
-                lblWelcome.Text = "Welcome Back " + customer.CustFirstName + " " + customer.CustLastName;
-                bookings = TravelExpertsDB.GetBookings(customer.CustomerId);
-                Session["Bookings"] = bookings;
-
-                decimal totalCost = 0;
-                foreach (var book in bookings)
-                {
-                    totalCost += book.GetTotal();
-                }
-                txtTotalCost.Text = totalCost.ToString("c");
-            }
-            else
-            {
-                Response.Redirect("Default.aspx");
-            }
-        }
-
-        protected void gvBookings_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            int index = gvBookings.SelectedIndex;
-            Booking booking = bookings[index];
-            details = TravelExpertsDB.GetBookingDetails(booking.BookingId);
-
-            decimal bookingTotal = 0;
-            foreach (var detail in details)
-            {
-                bookingTotal += detail.GetTotal();
-            }
-            txtBookingTotal.Text = bookingTotal.ToString("c");
         }
     }
 }
--- ASP Workshop/Site.Master.designer.cs
+++ ASP Workshop/Site.Master.designer.cs
@@ -29,7 +29,7 @@ namespace ASP_Workshop {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-
+        
         /// <summary>
         /// CustomerMenu control.
         /// </summary>
@@ -38,7 +38,7 @@ namespace ASP_Workshop {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl CustomerMenu;
-
+        
         /// <summary>
         /// LogoutOption control.
         /// </summary>
@@ -47,17 +47,7 @@ namespace ASP_Workshop {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl LogoutOption;
-
-        /// <summary>
-        /// linkLogout control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-
-        protected global::System.Web.UI.WebControls.Label linkLogout;
-
+        
         /// <summary>
         /// btnLogout control.
         /// </summary>
